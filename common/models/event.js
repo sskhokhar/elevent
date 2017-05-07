c8acbbfbc701277f72756d8a057bc2470483300b
Shahrukh Shahid <Shahrukh Shahid>
2017-05-07 19:57:54 +0500

    email

--- common/models/event.js
+++ common/models/event.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(Event) {
-
+    Event.prototype.invite = function(details, callback) {
+        var response;
+        // TODO
+        callback(null, response);
+    };
 };
