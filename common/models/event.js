e4d30ec955d9d0c47c38a216827fe1ed2c63ddfd
Shahrukh Shahid <Shahrukh Shahid>
2017-05-07 22:26:39 +0500

    email

--- common/models/event.js
+++ common/models/event.js
@@ -3,7 +3,17 @@
 module.exports = function(Event) {
     Event.prototype.invite = function(details, callback) {
         var response;
-        // TODO
-        callback(null, response);
+        console.log(details);
+        Event.app.models.email.send({
+            to: 'shah.khokhar@hotmail.com',
+            from: 'you@gmail.com',
+            subject: 'my subject',
+            text: 'my text',
+            html: 'my <em>html</em>'
+        }, function(err, mail) {
+            console.log('email sent!');
+            callback(err);
+        });
+        //callback(null, response);
     };
 };
