54784a30e603d58b568f6b4c0842c7ef3c5f9711
Shahrukh Shahid <Shahrukh Shahid>
2017-05-11 16:51:30 +0500

    Setup Email

--- common/models/event.js
+++ common/models/event.js
@@ -4,12 +4,19 @@ module.exports = function(Event) {
     Event.prototype.invite = function(details, callback) {
         var response;
         console.log(details);
+        var name = details.name;
+        var eventName = details.eventName;
+        var token = details.token;
         Event.app.models.email.send({
             to: 'shah.khokhar@hotmail.com',
             from: 'bilalaftabmalik@gmail.com',
-            subject: 'my subject',
-            text: 'my text',
-            html: 'my <em>html</em>'
+            subject: 'Invitation for collaboration.',
+            html: `Hi,
+            ${name} has invited you to collaborate on his event ${eventName}. Click <a href="http://52.15.198.192:3000/#!/signup?token=${token}"><u>here</u></a> or copy and paste this link in your browser</br>
+            http://52.15.198.192:3000/#!/signup?token=${token}
+            </br></br>
+            Thanks,
+            `
         }, function(err, mail) {
             console.log(err);
             callback(err);
