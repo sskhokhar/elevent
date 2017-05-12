f91ccfd5c806503e4773d0ef6f354cf87dd2d3b3
Shahrukh Shahid <Shahrukh Shahid>
2017-05-12 16:33:34 +0500

    GetCollaboratings

--- common/models/eventmanager.js
+++ common/models/eventmanager.js
@@ -2,4 +2,31 @@
 
 module.exports = function(Eventmanager) {
     //Eventmanager.settings.acls.length = 0;
+    var Event = Eventmanager.app.models.Event;
+    Eventmanager.prototype.getCollaboratings = function(callback) {
+        var events = [];
+        var count = 0;
+        var cIds = this.collaboratingIds;
+        if (cIds.length > 0) {
+            cIds.forEach(function(id) {
+                Event.findById(id, function(err, success) {
+                    if (!err) {
+                        events.push(success);
+                        count++;
+                        if (count == cIds.length) {
+                            callback(null, events);
+                        }
+                    } else {
+                        count++;
+                        if (count == cIds.length) {
+                            callback(err, null);
+                        }
+                    }
+                })
+            }, this);
+        } else {
+            callback(null, []);
+        }
+
+    };
 };
