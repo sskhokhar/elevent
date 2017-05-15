18e5b6246347244acdd7d3bb183459bf8db7ffca
Shahrukh Shahid <Shahrukh Shahid>
2017-05-15 16:34:58 +0500

    updated email system

--- common/models/event.js
+++ common/models/event.js
@@ -8,23 +8,51 @@ module.exports = function(Event) {
         var fromName = details.fromName;
         var eventName = this.title;
         var token = details.token;
-        Event.app.models.email.send({
-            to: 'shah.khokhar@hotmail.com',
-            from: 'bilalaftabmalik@gmail.com',
-            subject: 'Invitation for collaboration.',
-            html: `Hi ${toName},
-            ${fromName} has invited you to collaborate on the event <b>${eventName}</b>. Click <a href="http://52.15.198.192:3000/#!/signup?token=${token}"><u>here</u></a> or copy and paste this link in your browser</br>
-            http://52.15.198.192:3000/#!/signup?token=${token}
-            </br></br>
-            Thanks,
-            `
-        }, function(err, mail) {
+        var toEmail = details.toEmail;
+        var fromEmail = details.fromEmail;
+        var Eventmanager = Event.app.models.Eventmanager;
+        Eventmanager.find({ where: { email: toEmail } }, function(err, success) {
             if (!err) {
-                callback(null, mail)
-            } else {
-                callback(err, null);
+                if (success.length > 0) {
+                    Event.app.models.email.send({
+                        to: toEmail,
+                        from: fromEmail,
+                        subject: 'Invitation for collaboration.',
+                        html: `Hi ${toName},
+                            ${fromName} has invited you to collaborate on the event <b>${eventName}</b>. Click <a href="http://52.15.198.192:3000/#!/?token=${token}"><u>here</u></a> or copy and paste this link in your browser</br>
+                            http://52.15.198.192:3000/#!/signup?token=${token}
+                            </br></br>
+                            Thanks,
+                            `
+                    }, function(err, mail) {
+                        if (!err) {
+                            callback(null, mail)
+                        } else {
+                            callback(err, null);
+                        }
+                    });
+                } else {
+                    Event.app.models.email.send({
+                        to: toEmail,
+                        from: fromEmail,
+                        subject: 'Invitation for collaboration.',
+                        html: `Hi ${toName},
+                        ${fromName} has invited you to collaborate on the event <b>${eventName}</b>. Click <a href="http://52.15.198.192:3000/#!/signup?token=${token}"><u>here</u></a> or copy and paste this link in your browser</br>
+                        http://52.15.198.192:3000/#!/signup?token=${token}
+                        </br></br>
+                        Thanks,
+                        `
+                    }, function(err, mail) {
+                        if (!err) {
+                            callback(null, mail)
+                        } else {
+                            callback(err, null);
+                        }
+                    });
+                }
             }
-        });
+        })
+
         //callback(null, response);
     };
 };
